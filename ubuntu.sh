74e009da0842d0c52c737762290ccdffda06d94c
Tinson Lai <laitinson@gmail.com>
2019-06-09 18:58:45 +1000

    Integrate auxiliary files into one script

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,34 +1,81 @@
 #!/bin/bash
 
-source root-check.sh || exit $?
+# colours
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+# logging levels
+INFO="${YELLOW}INFO${NC}"
+WARNING="${RED}WARNING${NC}"
+
+info_echo() {
+    printf "$INFO: $*\n"
+}
+
+info_printf() {
+    printf "$INFO: $*"
+}
+
+warning_echo() {
+    printf "$WARNING: $*\n"
+}
+
+warning_printf() {
+    printf "$WARNING: $*"
+}
+
+# check if current is root
+if [[ $EUID -ne 0 ]]
+then
+    warning_printf "This script must be run as root, use \"sudo $0"
+    [[ ! -z $@ ]] && printf " $*"
+    echo "\" instead"
+    return 1
+fi
+
 
 if uname -a | grep -q Microsoft
 then
     # add wsl.conf to /etc to enable permission bits on NTFS for WSL
-    curl https://gitlab.com/snippets/1864926/raw > wsl.conf
-    install -o root -g root -m 644 ./wsl.conf /etc/
+    cat > wsl.conf <<- EOL
+[automount]
+enabled = true
+root = /mnt/
+options = "metadata,fmask=0133,dmask=0022"
+mountFsTab = true
+
+[network]
+generateHosts = true
+generateResolvConf = true
+
+[interop]
+enabled = true
+appendWindowsPath = true
+EOL
+    info_echo "Adding wsl.conf to /etc"
+    install -o root -g root -m 644 wsl.conf /etc/
     rm wsl.conf
 
     # containerisation is not supported yet
+    info_echo "Removing lxd from the system for WSL"
     apt-get -y remove lxd lxd-client
 fi
 
 print_usage() {
     echo "Usage: (sudo) $0 -m -u"
-    echo "    -m - install Miniconda"
-    echo "    -u - update ubuntu"
 }
 
-miniconda=false
-update=false
+location=au
+distro=disco
 
-while getopts ":mu" opt; do
+while getopts ":d:l:mu" opt; do
     case $opt in
-        m )
-            miniconda=true
+        d )
+            distro=$OPTARG
             ;;
-        u )
-            update=true
+        l )
+            location=$OPTARG
             ;;
         \? )
             print_usage
@@ -42,30 +89,53 @@ while getopts ":mu" opt; do
     esac
 done
 
-# change to au server
-sed -i "s/\/archive\.ubuntu/\/au\.archive\.ubuntu/g" /etc/apt/sources.list
+# create a sources.list file
+rm sources.list
+for pool in $distro $distro-updates $distro-backports $distro-security
+do
+    echo "deb http://$location.archive.ubuntu.com/ubuntu/" $pool main restricted universe multiverse >> sources.list
+done
+echo "deb http://archive.canonical.com/ubuntu $distro partner" >> sources.list
+cp /etc/apt/sources.list /etc/apt/sources.list.backup
+install -o root -g root -m 644 sources.list /etc/apt/
+rm sources.list
 
+info_echo "Adding Miniconda repo"
+# first install the gpg key
+curl https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc | gpg --dearmor > conda.gpg
+install -o root -g root -m 644 conda.gpg /etc/apt/trusted.gpg.d/
+# add the conda repo
+echo "deb [arch=amd64] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" > /etc/apt/sources.list.d/conda.list
 
-[[ -z $update ]] || info_echo "Upgrading to new release" && do-release-upgrade -q
-
-if [ $miniconda = true ]
-then
-    info_echo "Installing Miniconda 3"
-
-    # first install the gpg key
-    curl https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc | gpg --dearmor > conda.gpg
-    install -o root -g root -m 644 conda.gpg /etc/apt/trusted.gpg.d/
-
-    # add the conda repo
-    echo "deb [arch=amd64] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" > /etc/apt/sources.list.d/conda.list
-fi
-
+info_echo "Refreshing the index and installing/upgrading packages"
 apt-get update
-curl https://gitlab.com/snippets/1864928/raw 2> /dev/null | apt-get -y install
-apt-get -y install conda
+# generate the list
+cat > apt.list <<- EOL
+gcc
+g++
+make
+libboost-dev-all
+clang
+llvm
+valgrind
+gdb
+lldb
+openjdk-8-jdk
+openjdk-11-jdk
+haskell-platform
+git
+nano
+moreutils
+htop
+net-tools
+expect
+tree
+conda
+EOL
+apt-get -y install < apt.list
+rm apt.list
+# Upgrade the rest
 apt-get -y dist-upgrade
 apt-get -y upgrade
 
-# Clone nano improved syntax highlight file
-git clone https://github.com/scopatz/nanorc.git .nano
-ln -s .nano/nanorc .nanorc
+info_echo "Script finalised"
