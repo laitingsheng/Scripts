fa0475971f97a5c68841a4d40b22ccec0890872e
Tinson Lai <laitingsheng@hotmail.com>
2020-09-21 02:07:06 +0930

    Add two more official apt repositories

--- ubuntu/lists/wsl.bash
+++ ubuntu/lists/wsl.bash
@@ -10,9 +10,16 @@ sed -i "s/%DIST%/${dist}/g" /etc/apt/sources.list.d/docker.list
 install -o root -g root -m 644 ${DIR}/lists/google.list /etc/apt/sources.list.d/google.list
 install -o root -g root -m 644 ${DIR}/lists/microsoft.list /etc/apt/sources.list.d/microsoft.list
 sed -i "s/%RELEASE%/${release}/g;s/%DIST%/${dist}/g" /etc/apt/sources.list.d/microsoft.list
+install -o root -g root -m 644 ${DIR}/lists/hashicorp.list /etc/apt/sources.list.d/hashicorp.list
+sed -i "s/%DIST%/${dist}/g" /etc/apt/sources.list.d/hashicorp.list
+install -o root -g root -m 644 ${DIR}/lists/github.list /etc/apt/sources.list.d/github.list
+sed -i "s/%DIST%/${dist}/g" /etc/apt/sources.list.d/github.list
 
 xargs apt-key adv -q --fetch-keys <<- EOL
 https://packages.microsoft.com/keys/microsoft.asc
 https://download.docker.com/linux/ubuntu/gpg
 https://packages.cloud.google.com/apt/doc/apt-key.gpg
+https://apt.releases.hashicorp.com/gpg
 EOL
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
--- ubuntu/wsl.bash
+++ ubuntu/wsl.bash
@@ -10,10 +10,27 @@ source ${DIR}/lists/refresh.bash
 # Remove LXC & Snap for WSL
 apt-get purge -fy lxd lxd-client snapd
 
+# System
 xargs apt-get install -fy <<- EOL
 ubuntu-minimal
 ubuntu-standard
 ubuntu-server
+ubuntu-wsl
+wsl
+locales-all
+language-pack-en
+language-pack-zh-hans
+language-pack-zh-hant
+fonts-noto
+errno
+parallel
+expect
+tree
+p7zip-full
+EOL
+
+# Development
+xargs apt-get install -fy <<- EOL
 build-essential
 cmake-extras
 meson
@@ -29,24 +46,22 @@ bison
 git-all
 git-ftp
 git-lfs
+gh
 subversion
 ruby-all-dev
-wsl
-errno
-parallel
-expect
-tree
-p7zip
 python3-all-dbg
-python3-keras
 python3-pip
 python3-venv
+python3-pygments
+python3-coverage-test-runner
+python3-git
+python3-autopep8
+mypy
+cython3-dbg
+pycodestyle
+jupyter
 gradle
 maven
-libboost-all-dev
-libyaml-cpp-dev
-libfmt-dev
-libantlr4-runtime-dev
 openjdk-8-jdk
 openjdk-11-jdk
 haskell-platform
@@ -66,17 +81,58 @@ texlive-full
 gccgo
 golang
 gnugo
-nodejs
 npm
 julia
-locales-all
-language-pack-en
-language-pack-zh-hans
-language-pack-zh-hant
+nvidia-cuda-toolkit-gcc
+terraform
+EOL
+
+# Libraries
+xargs apt-get install -fy <<- EOL
+libboost-all-dev
+libyaml-cpp-dev
+libfmt-dev
 intel-mkl-full
+python3-unidiff
+python3-ddt
+python3-keras
+python3-sklearn-pandas
+python3-skimage
+python3-seaborn
+python3-matplotlib-dbg
+python3-py
+python3-pytools
+python3-selenium
+python3-pythonmagick
+python3-mpi4py-dbg
+python3-openstacksdk
+python3-openstackclient
+node-react
+node-typescript-types
+EOL
+
+# Apps
+xargs apt-get install -fy <<- EOL
+azure-cli
+dotnet-sdk-*
+docker-ce
+google-cloud-sdk
+kubeadm
+EOL
+
+# Docs
+xargs apt-get install -fy <<- EOL
+gccgo-doc
+gcc-doc
+haskell-platform-doc
+julia-doc
+libboost-doc
+libfmt-doc
+libtbb-doc
+mypy-doc
+openjdk-8-doc
+openjdk-11-doc
+python3-doc
 EOL
 
 install -o root -g root -m 644 wsl.conf /etc/wsl.conf
-apt-get install -fy ubuntu-wsl
-
-source dev.bash
