2706901d747ef12f632cd8c2d090cd9288d39162
Tinson Lai <laitingsheng@hotmail.com>
2021-02-24 00:52:10 +1030

    Prune folder

--- init/conda.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-wget -O /tmp/miniconda3.sh 'https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh'
-sh /tmp/miniconda3.sh -bfut
-rm /tmp/miniconda3.sh
-
-~/miniconda3/bin/conda init
